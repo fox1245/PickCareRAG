96d46a4f0329ba5979b6cd801a2338fa562b4c95
fox1245 <procoinmac0@gmail.com>
2025-07-28 13:09:49 +0900

    editted

--- WebBaseLoader.py
+++ WebBaseLoader.py
@@ -3,12 +3,12 @@ class WebBaseLoader():
     def __init__(self, url):
         self.web_paths = [url,]
     
-    def parse(self, attrArgs: dict):
+    def parse(self, attrArgs: dict, klass : str):
         loader = init.WebBaseLoader(
             web_paths = self.web_paths,
             bs_kwargs = dict(
                 parse_only = init.bs4.SoupStrainer(
-                    "div",
+                    klass,
                     attrs=attrArgs,
 
                 )
