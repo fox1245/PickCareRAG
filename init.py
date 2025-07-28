ff4317b9222ed9923088699ba782ed90e1b33c90
fox1245 <procoinmac0@gmail.com>
2025-07-28 12:14:50 +0900

    editted

---
+++ init.py
@@ -0,0 +1,10 @@
+# API 키를 환경변수로 관리하기 위한 설정 파일
+from dotenv import load_dotenv
+from langchain_teddynote import logging
+import bs4
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.vectorstores import Chroma, FAISS
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings
