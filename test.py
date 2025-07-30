e9dc7b9f68c4321f8ea2b639b8051e9afa7d7f8f
fox1245 <procoinmac0@gmail.com>
2025-07-30 11:31:26 +0900

    modified

--- test.py
+++ test.py
@@ -146,10 +146,91 @@ def simpleChatWithHistory(ask):
     return response
 
 
+def RAG_RunnableWithMessageHistory(file_path, ask : dict,  model = "gpt-4o-mini",temp = 0,  k=3, chunk_size = 1000, chunk_overlap = 50):
+    #문서 로드
+    loader = PDF.pdfLoader(file_path=file_path, extract_bool= True)
+    docs = loader.load()
+    #문서 분할
+    text_splitter = init.RecursiveCharacterTextSplitter(chunk_size = chunk_size, chunk_overlap = chunk_overlap)
+    split_documents = text_splitter.split_documents(docs)
+    #임베딩 생성
+    embeddings = init.OpenAIEmbeddings()
+    
+    #DB생성 및 저장
+    vectorstore = init.FAISS.from_documents(documents= split_documents, embedding= embeddings)
+    
+    #검색기 (Retriever) 생성
+    retriever = vectorstore.as_retriever(search_kwargs = {"k" : k})
+    
+    #프롬프트 생성
+
+    
+    system = ask['system']
+    prompt = init.PromptTemplate.from_template(
+            """
+                You are an assistant for question-answering tasks. 
+                Use the following pieces of retrieved context to answer the question. 
+                If you don't know the answer, just say that you don't know. 
+                Answer in Korean.
+
+
+            #Previous Chat History:
+            {chat_history}
+
+            #Question: 
+            {question} 
+
+            #Context: 
+            {context} 
+
+            #Answer:"""
+    )
+    
+    
+    #모델 생성
+    llm = init.ChatOpenAI(model_name = model, temperature= temp)
+    
+    #체인 생성
+    chain = (
+        {
+            "context" : init.itemgetter("question") | retriever,
+            "question" : init.itemgetter("question"),
+            "chat_history" : init.itemgetter("chat_history"),
+        }
+        | prompt
+        | llm
+        | init.StrOutputParser()
+    )
+    
+    global store
+    store = dict()
+    
+    
+    rag_with_history = init.RunnableWithMessageHistory(
+        chain, 
+        get_session_history, #세션 기록을 가져오는 함수
+        input_messages_key= "question", #사용자의 질문이 템플릿 변수에 들어갈 key
+        history_messages_key= "chat_history",  #기록 메시지의 키
         
+    )
+    
+    response = rag_with_history.invoke(
+        #질문 입력
+        {"question" : ask["question"]},
+        config = {"configurable" : {"session_id" : "rag123"}},
+    )
+    
+    return response
+
     
     
     
+    
+    
+
+
+        
+
 
 
 
@@ -164,14 +245,15 @@ if __name__ == "__main__":
     # for elem in docs:
     #     print(elem.page_content)
     # QA = "삼성 가우스에 대해 설명해주세요"
-    # file_path = "data/SPRI_AI_Brief_2023년12월호_F.pdf"
+    file = "data/SPRI_AI_Brief_2023년12월호_F.pdf"
     # pdfQuery = PDFask(model = "gpt-4o-mini", QA = QA, file_path = file_path)
     # for elem in pdfQuery:
     #     print(elem)
     global store
     store = {}
-    ask = {'system': '당신은 Question-Answering 챗봇입니다. 주어진 질문에 대한 답변을 제공해주세요.', 'question': '저는 개발자 입니다.'}
-    response = simpleChatWithHistory(ask)
+    ask = {'system': '당신은 Question-Answering 챗봇입니다. 주어진 질문에 대한 답변을 제공해주세요.', 'question': '주어진 자료에서 핵심 사항을 요약해서 알려주세요'}
+    #response = simpleChatWithHistory(ask)
+    response = RAG_RunnableWithMessageHistory(file_path = file, ask = ask)
     print(response)
     
     
